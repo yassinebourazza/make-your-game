4d8da4bcca919a30d65a29eca2b8ad2fa65254d9
yassinebourazza <nite246135@gmail.com>
2026-01-11 08:17:50 +0100

    add lives

--- js/game.js
+++ js/game.js
@@ -4,32 +4,46 @@ let currentX = 0
 let currentY = 0
 let moveY = -5
 let moveX = 5
+let lives = 3
 let flag = true
 let layers = {}
 
-let container;
-let ball;
-let breaker;
-let breakerPlace;    
-let containerPlace;
+let container, ball, breaker, breakerPlace, ballPlace, containerPlace, heartLives;
 
+let moveLeft = false;
+let moveRight = false;
 
 let play = document.getElementById('play')
-play.addEventListener('click', ()=> {
-    started()
-})
+
+
 function started() {
-    
+
     document.body.innerHTML = `
-    <div id="container">
-       <div id="ball"></div>
-       <div id="breaker"></div>
+    <div id="game">
+        <header>
+            <span><p>High Score</p><p>Score</p><p>Time</p></span>
+            <span><p>0</p><p>15</p><p>10:00</p></span>
+        </header>
+        <div id="container">
+          <div id="ball"></div>
+          <div id="breaker"></div>
+       </div>
+       <div id="control">
+        <div><span>lives :</span>
+        <img class="lives" src="assets/heart.svg" alt="heart">
+        <img class="lives" src="assets/heart.svg" alt="heart">
+        <img class="lives" src="assets/heart.svg" alt="heart">
+        </div>
+        <div>
+        <button id="pause-button"><img src="assets/pause.svg" alt="pause"><p>Pause</p></button>
+        <button id="home-button"><img src="assets/home.svg" alt="home"><p>Home</p></button></div>
+       </div>
     </div>
    `
-
     container = document.getElementById('container')
     ball = document.getElementById('ball')
     breaker = document.getElementById('breaker')
+    heartLives = document.getElementsByClassName('lives')
     breakerPlace = breaker.getBoundingClientRect()
     containerPlace = container.getBoundingClientRect()
     layers = BrickGenerator()
@@ -40,39 +54,44 @@ function started() {
 }
 
 
-addEventListener('keydown', (e)=> {
-    if (e.key == ' ' && flag) {
-        flag = !flag
-        requestAnimationFrame(BallMovement)
-        
-    }
-})
-
-
-
-
-let moveLeft = false;
-let moveRight = false;
-
-document.addEventListener('keydown', e => {
-    if(e.key === 'ArrowRight') moveRight = true;
-    if(e.key === 'ArrowLeft') moveLeft = true;
-});
-
-document.addEventListener('keyup', e => {
-    if(e.key === 'ArrowRight') moveRight = false;
-    if(e.key === 'ArrowLeft') moveLeft = false;
-});
-
 function BallMovement() {
     
-    let ballPlace = ball.getBoundingClientRect()
+    ballPlace = ball.getBoundingClientRect()
     breakerPlace = breaker.getBoundingClientRect()
     ball.style.transform = `translate(${currentX+moveX}px,${currentY+moveY}px)`
     currentX += moveX
     currentY += moveY
     
-    if (ballPlace.y + ballPlace.height > breakerPlace.y && ballPlace.y < breakerPlace.y  
+    ballTouchTheBreaker()
+   
+    if (ballPlace.x > containerPlace.x+(containerPlace.width-2)-(ballPlace.width)) {
+        moveX = moveX < 0 ? moveX : -moveX
+    } else if (ballPlace.x <containerPlace.x) {
+        moveX = moveX > 0 ? moveX : -moveX
+    } else if (ballPlace.y < containerPlace.y) {
+        moveY = moveY > 0 ? moveY : -moveY
+    } else if (ballPlace.y + ballPlace.height > containerPlace.y+(containerPlace.height)-(ballPlace.height/2)) {
+       lives--
+        if (lives >0) {
+            ball.style.transform = `translate(0px,0px)`
+            moveX = 5
+            moveY = -5
+            currentX = 0
+            currentY = 0
+            heartLives[lives].remove()
+       } else {
+        heartLives[lives].remove()
+        return
+       }
+    }
+
+    ballTouchTheBricks()
+    breakerMove()
+    requestAnimationFrame(BallMovement)
+}
+
+function ballTouchTheBreaker() {
+     if (ballPlace.y + ballPlace.height > breakerPlace.y && ballPlace.y < breakerPlace.y  
         && ballPlace.x + (ballPlace.width/2) > breakerPlace.x 
         && ballPlace.x + (ballPlace.width/2) < breakerPlace.x+breakerPlace.width) {
         if (ballPlace.x + (ballPlace.width/2) < breakerPlace.x+(breakerPlace.width*1/7)) {
@@ -92,19 +111,6 @@ function BallMovement() {
         }
         
     }
-    if (ballPlace.x > containerPlace.x+(containerPlace.width-2)-(ballPlace.width)) {
-        moveX = moveX < 0 ? moveX : -moveX
-    } else if (ballPlace.x <containerPlace.x) {
-        moveX = moveX > 0 ? moveX : -moveX
-    } else if (ballPlace.y < containerPlace.y) {
-        moveY = moveY > 0 ? moveY : -moveY
-    } else if (ballPlace.y > containerPlace.y+(containerPlace.height-2)-(ballPlace.height)) {
-        return
-    }
-
-    ballTouchTheBricks(ballPlace)
-    breakerMove()
-    requestAnimationFrame(BallMovement)
 }
 
 function breakerMove() {
@@ -118,7 +124,7 @@ function breakerMove() {
 }
 
 
-function ballTouchTheBricks(ballPlace) {
+function ballTouchTheBricks() {
     for (let layer in layers) {
         if (ballPlace.y > layers[layer].yHeight) return
         if (ballPlace.y+ballPlace.height > layers[layer].y) {
@@ -130,12 +136,12 @@ function ballTouchTheBricks(ballPlace) {
                     moveY = -moveY
                 }
                 if (ballPlace.y+(ballPlace.width/2) < layers[layer].yHeight && ballPlace.y+(ballPlace.width/2) > layers[layer].y) {
-                    if ((ballPlace.x+(ballPlace.width) > br.x && ballPlace.x < br.x)
+                if ((ballPlace.x+(ballPlace.width) > br.x && ballPlace.x < br.x)
                         ||(ballPlace.x < br.x+br.width && ballPlace.x+ballPlace.width > br.x+br.width)) {
                         current.remove()
                         console.log(br,ballPlace, 'move x');
                         moveX = -moveX
-                    }
+                }
                 }
             
             }
@@ -144,3 +150,27 @@ function ballTouchTheBricks(ballPlace) {
         
     }    
 }
+
+
+
+addEventListener('keydown', (e)=> {
+    if (e.key == ' ' && flag) {
+        flag = !flag
+        requestAnimationFrame(BallMovement)
+        
+    }
+})
+
+document.addEventListener('keydown', e => {
+    if(e.key === 'ArrowRight') moveRight = true;
+    if(e.key === 'ArrowLeft') moveLeft = true;
+});
+
+document.addEventListener('keyup', e => {
+    if(e.key === 'ArrowRight') moveRight = false;
+    if(e.key === 'ArrowLeft') moveLeft = false;
+});
+
+play.addEventListener('click', ()=> {
+    started()
+})
